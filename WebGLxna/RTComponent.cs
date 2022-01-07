df53a86363ce368b9d444e37e9af8c8528297baa
Nick Kastellanos <nkastellanos@gmail.com>
2022-01-07 07:57:30 +0200

    Draw on rendertarget

--- WebGLxna/RTComponent.cs
+++ WebGLxna/RTComponent.cs
@@ -21,7 +21,7 @@ namespace WebGLxna
 
 
         protected override void LoadContent()
-        {           
+        {
             _sb = new SpriteBatch(GraphicsDevice);
             _rt = new RenderTarget2D(GraphicsDevice, 128, 128, false, SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.PlatformContents);
             _tx = _content.Load<Texture2D>("Icon192");
@@ -33,6 +33,12 @@ namespace WebGLxna
             GraphicsDevice.SetRenderTarget(_rt);
             GraphicsDevice.Clear(Color.DarkOrange);
 
+            _sb.Begin();
+            _sb.Draw(_tx, new Vector2(0,0), null, Color.White, 0, Vector2.Zero, 0.2f, SpriteEffects.None, 0);
+            _sb.Draw(_tx, new Vector2(50,50), null, Color.White, 2, Vector2.Zero, 0.2f, SpriteEffects.None, 0);
+            _sb.End();
+
+            GraphicsDevice.SetRenderTarget(null);
         }
 
         public override void Draw(GameTime gameTime)
